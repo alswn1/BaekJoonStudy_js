1b72059c4f93b76711132bbd26e3e5a9b302a2f6
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-08-21 16:04:22 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 100 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -0,0 +1,13 @@
+let path = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = require('fs').readFileSync(path).toString().trim().split(/r?\n/);
+
+var str = input[0];
+var al = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var answer = "";
+
+al.map((e) => {
+    answer += str.indexOf(e) + " ";
+})
+
+answer = answer.trim();
+console.log(answer);
