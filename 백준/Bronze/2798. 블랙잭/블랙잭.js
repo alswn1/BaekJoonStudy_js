e31d12fd38b2a429156ab290736c4eeae85d673e
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-08-22 14:02:45 +0900

    [Bronze II] Title: 블랙잭, Time: 152 ms, Memory: 9608 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.js
+++ 백준/Bronze/2798. 블랙잭/블랙잭.js
@@ -1,20 +1,15 @@
 let path = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = require('fs').readFileSync(path).toString().trim().split(/r?\n/);
 
-var len = parseInt(input[0].split(' ')[0]);
-var sum = parseInt(input[0].split(' ')[1]);
-var numArr = input[1].split(' ');
+var [len, sum] = input[0].split(' ').map((e) => parseInt(e));
+var arr = input[1].split(' ').map((e) => parseInt(e));
 var answer = 0;
 
 for (let i=0; i<len-2; i++) {
     for (let j=i+1; j<len-1; j++) {
         for (let k=j+1; k<len; k++) {
-            var a = parseInt(numArr[i]);
-            var b = parseInt(numArr[j]);
-            var c = parseInt(numArr[k]);
-            if(a+b+c <= sum && a+b+c > answer) {
-                answer = a+b+c;
-            }
+            var a = arr[i] + arr[j] + arr[k];
+            if(a <= sum && a > answer) answer = a;
         }
     }
 }
