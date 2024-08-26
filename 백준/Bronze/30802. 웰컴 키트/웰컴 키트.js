c29e6997fbd15c94c997897b1ce4d5d8dc00e8dc
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-08-26 14:25:17 +0900

    [Bronze III] Title: 웰컴 키트, Time: 92 ms, Memory: 9592 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.js
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.js
@@ -2,23 +2,17 @@ let path = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = require('fs').readFileSync(path).toString().trim().split(/r?\n/);
 
 let stu = parseInt(input[0]);
-let shirts = input[1].split(" ").map((e)=> parseInt(e));
-let shirtsLength = shirts.length;
+let shirts = input[1].split(" ").map(Number);
 let t = input[2].split(" ")[0];
 let p = input[2].split(" ")[1];
 
-let answer1 = 0;
-let answer2 = "";
+let tshirtsBundles = 0;
+shirts.map((e) => {
+    tshirtsBundles += Math.ceil(e / t);
+})
 
-for (let i=0; i<shirtsLength; i++) {
-    if (shirts[i] % t != 0) {
-        answer1 += Math.floor(shirts[i] / t) + 1;
-    }else {
-        answer1 += Math.floor(shirts[i] / t);
-    }
-}
+let penBundles = Math.floor(stu / p);
+let extraPens = stu % p;
 
-answer2 = Math.floor(stu/p) + " " + (stu%p);
-
-console.log(answer1);
-console.log(answer2);
+console.log(tshirtsBundles);
+console.log(`${penBundles} ${extraPens}`);
