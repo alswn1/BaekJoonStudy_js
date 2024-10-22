ff1f86d0d770a6dab4d31cd8bde20917114380ae
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-10-22 11:03:51 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.34 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,51 +1,16 @@
 function solution(n, words) {
-    var answer = [];
-    var answer1 = [];
-    var peo = Array.from(Array(n), () => Array(0).fill());
+    let turns = Array(n).fill(0), history = {};
     
-    var num = 0;
     for (let i=0; i<words.length; i++) {
-        num = i % n;
-        peo[num].push(words[i]);
-    
-        if (i > 0 && words[i].charAt(0) != words[i-1].charAt(words[i-1].length-1)) {
-            answer1[0] = num+1;
-            answer1[1] = peo[num].length;
-            break;
-        }
-    }
-    
-    for (let i=0; i<peo[0].length; i++) {
-        var cc = 0;
-        for (let j=0; j<n; j++) {
-            cc = n * i + j;
-            if (cc > words.length-1) {
-                break;
-            }else {
-                var chk = words.indexOf(peo[j][i]);
-                if (chk !== n * i + j) {
-                    answer[0] = j + 1;
-                    answer[1] = i + 1;
-                    break;
-                }
-            }
-        }
-        if (answer.length != 0 || cc > words.length-1) {
-            break;
-        }
-    }
-    
-    if (answer.length == 0 && answer1.length == 0) {
-        return [0, 0];
-    }else if (answer.length == 0 && answer1.length != 0) {
-        return answer1;
-    }else if (answer1.length == 0) {
-        return answer;
-    }else {
-        var a = n * (answer[1]-1) + answer[0];
-        var b = n * (answer1[1]-1) + answer1[0];
+        let person = i % n, word = words[i];
+        turns[person]++;
 
-        if (a < b) return answer;
-        else return answer1;
+        if (i > 0 && (word[0] != words[i-1].slice(-1) || history[word])) {
+            return [person + 1, turns[person]];
+        }
+
+        history[word] = true;
     }
+
+    return [0, 0];
 }
