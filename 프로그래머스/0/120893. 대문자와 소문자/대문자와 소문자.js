adc0d029b9f8b20e6489fb27ff7252a3b92a6d68
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-10-24 13:57:52 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.45 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,10 +1,8 @@
 function solution(my_string) {
     var answer = "";
     
-    for (let i=0; i < my_string.length; i++) {
-        var charCode = my_string.charAt(i).charCodeAt();
-        if (charCode <= 90) answer += String.fromCharCode(charCode + 32);
-        else answer += String.fromCharCode(charCode - 32);
+    for (let c of my_string) {
+        answer += c === c.toLowerCase() ? c.toUpperCase() : c.toLowerCase();
     }
     
     return answer;
