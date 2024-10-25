6db9a98e96993f719772ecf92599937ced84e0fa
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-10-25 13:25:12 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,4 +1,4 @@
 function solution(s) {
-    if ((s.length == 4 || s.length == 6) && /^[0-9]+$/.test(s)) return true;
+    if ((s.length == 4 || s.length == 6) && /^\d+$/.test(s)) return true;
     else return false;
 }
