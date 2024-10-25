9004cc19df4ca5a94105a0315d9f0b08ff09fed3
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-10-25 15:31:28 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,8 +1,3 @@
 function solution(n) {
-    var answer = 0;
-
-    var three = n.toString(3);
-    answer = parseInt(three.split('').reverse().join(''), 3);
-
-    return answer;
+    return parseInt([...n.toString(3)].reverse().join(''), 3);
 }
