36cc533ed596c72e69b48bab8d9fafe6859c1869
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-10-25 13:40:19 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 52.81 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,7 +1,8 @@
 function solution(arr) {
     var answer = [arr[0]];
+    var len = arr.length;
 
-    for (let i=1; i<arr.length; i++) {
+    for (let i=1; i<len; i++) {
         if (answer[answer.length-1] != arr[i]) answer.push(arr[i]);
     }
 
