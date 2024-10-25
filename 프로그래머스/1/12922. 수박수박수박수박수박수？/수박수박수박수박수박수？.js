02cac286f2ad6ccc06ca6bb9aec76c85e8f0bd10
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-10-25 10:34:47 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,10 +1,1 @@
-function solution(n) {
-    var answer = "";
-
-    for (let i = 0; i < n; i++) {
-        if (i % 2 == 0) answer += "수";
-        else answer += "박";
-    }
-
-    return answer;
-}
+var solution = n => '수박'.repeat(n/2) + (n % 2 == 1 ? '수' : '');
