8d15f9580fb306ceefd13513be26de625d91d9f5
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-10-28 11:02:41 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,16 +1,3 @@
-function solution(array, n) {
-    array.sort((a, b) => a - b);
-    let index = array.indexOf(n);
-    let count = 0;
-    
-    if (index != -1){
-        let len = array.length;
-        
-        for (let i=index; i<len; i++) {
-            if (array[i] == n) count++;
-            else break;
-        }
-    }
-    
-    return count;
+function solution(array, n) {    
+    return array.filter((e) => e == n).length;
 }
