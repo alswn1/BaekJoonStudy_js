2eff5e88977e9b90df07c0935c2dfd5beae86a7e
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-10-28 15:59:17 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 59.23 ms, Memory: 138 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+    var answer = [];
+    arr.map((e) => {
+        if (e >= 50 && e % 2 == 0) answer.push(e/2);
+        else if (e < 50 && e % 2 != 0) answer.push(e*2);
+        else answer.push(e);
+    })
+    
+    return answer;
+}
