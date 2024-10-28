237421157423d4c3391f536442289ddd2de1a0aa
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-10-28 17:35:54 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -1,10 +1,3 @@
 function solution(my_string, index_list) {
-    let answer = '';
-    let len = index_list.length;
-    
-    for (let i=0; i<len; i++) {
-        answer += my_string.charAt(index_list[i]);
-    }
-    
-    return answer;
+    return index_list.map((e) => my_string[e]).join('');
 }
