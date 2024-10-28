edd49f96440f7d210da6fc516c666e540ed60c54
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-10-28 16:11:08 +0900

    [level 0] Title: 할 일 목록, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
@@ -1,10 +1,3 @@
 function solution(todo_list, finished) {
-    let len = todo_list.length;
-    let answer = [];
-    
-    for (let i=0; i<len; i++) {
-        if (finished[i] == false) answer.push(todo_list[i]);
-    }
-    
-    return answer;
+    return todo_list.filter((e, i) => !finished[i]);
 }
