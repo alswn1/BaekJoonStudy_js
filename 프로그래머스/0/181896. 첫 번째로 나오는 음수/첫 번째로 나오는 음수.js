9c9dc7c25dfc741fab53dc4a6cb270839bcdcbf8
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-10-28 16:52:28 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,9 +1,3 @@
 function solution(num_list) {
-    let len = num_list.length;
-    for (let i=0; i<len; i++) {
-        if (num_list[i] < 0) {
-            return i;
-        }
-    }
-    return -1;
+    return num_list.findIndex((e) => e<0);
 }
