5f72c001bd3d57cde2781027592a8473053f298b
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-10-29 09:51:26 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,6 +1,6 @@
 function solution(num_list) {
     let mul = num_list.reduce((a, c) => a * c, 1);
-    let sum = Math.pow((num_list.reduce((a, c) => a + c, 0)),2);
+    let sum = num_list.reduce((a, c) => a + c, 0);
     
-    return +(mul < sum);
+    return +(mul < sum ** 2);
 }
