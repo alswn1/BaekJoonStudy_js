f65f4c6e2c3b47fc765ff239d7586cc46e23cbaf
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-10-29 14:41:42 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.63 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -1,9 +1,3 @@
 function solution(arr) {
-    let len = arr.length;
-    for (let i=0; i<len; i++) {
-        for (let j=0; j<len; j++) {
-            if (arr[i][j] != arr[j][i]) return 0;
-        }
-    }
-    return 1;
+    return arr.some((rows, i) => rows.some((v, j) => v != arr[j][i])) ? 0 : 1;
 }
