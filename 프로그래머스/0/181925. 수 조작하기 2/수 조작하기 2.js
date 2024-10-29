ab7d8f9c1150af25450d8df27b6a04e1adb113ee
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-10-29 09:25:47 +0900

    [level 0] Title: 수 조작하기 2, Time: 11.41 ms, Memory: 45.7 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -1,13 +1,12 @@
+const controls = {
+    '1' : 'w',
+    '-1' : 's',
+    '10' : 'd',
+    '-10' : 'a'
+};
+
 function solution(numLog) {
-    let len = numLog.length;
-    let answer = "";
-    
-    for (let i=0; i<len-1; i++) {
-        if (numLog[i] + 1 == numLog[i+1]) answer += "w";
-        else if (numLog[i] - 1 == numLog[i+1]) answer += "s";
-        else if (numLog[i] + 10 == numLog[i+1]) answer += "d";
-        else if (numLog[i] - 10 == numLog[i+1]) answer += "a";
-    }
-    
-    return answer;
+    return numLog.reduce((a, c, i) => (
+        i === 0 ? a : a + controls[c - numLog[i-1]]
+    ),'')
 }
