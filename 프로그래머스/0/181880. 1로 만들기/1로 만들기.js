0b599bd27f0219dd5cc975d30a5f46e8e3a52e31
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-10-30 14:24:32 +0900

    [level 0] Title: 1로 만들기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -1,11 +1,13 @@
 function solution(num_list) {
-    return num_list.reduce((total, num) => {
-        let steps = 0;
+    return num_list.map((e) => e.toString(2).length - 1).reduce((a, c) => a + c);
+    
+//     return num_list.reduce((total, num) => {
+//         let steps = 0;
         
-        while (num != 1) {
-            num = num % 2 == 0 ? num / 2 : (num - 1) / 2;
-            steps++;
-        }
-        return total + steps;
-    }, 0);
+//         while (num != 1) {
+//             num = num % 2 == 0 ? num / 2 : (num - 1) / 2;
+//             steps++;
+//         }
+//         return total + steps;
+//     }, 0);
 }
