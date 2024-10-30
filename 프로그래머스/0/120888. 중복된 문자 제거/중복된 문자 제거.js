a7ac0f7e291400527fcb9463b63431a180648a87
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-10-30 10:47:51 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,8 +1,3 @@
 function solution(my_string) {
-    let arr = new Set();
-    for (let c of my_string) {
-        arr.add(c);
-    }
-    
-    return [...arr].join('');
+    return [...new Set(my_string)].join('');
 }
