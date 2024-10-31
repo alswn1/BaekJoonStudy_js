ad1fe42254f1734743f27dd75a25715f07a3e01c
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-10-31 11:33:24 +0900

    [level 0] Title: 이진수 더하기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
@@ -1,3 +1,23 @@
 function solution(bin1, bin2) {
-    return (parseInt(bin1, 2) + parseInt(bin2, 2)).toString(2);
+    // return (parseInt(bin1, 2) + parseInt(bin2, 2)).toString(2);
+    
+    let temp = Number(bin1) + Number(bin2);
+    temp = [...temp.toString()].reverse().map((e) => +e);
+    let len = temp.length;
+    
+    for (let i=len; i<11; i++) {
+        temp.push(0);
+    }
+    
+    for (let i=0; i<len; i++) {
+        if (temp[i] === 2) {
+            temp[i] = 0;
+            temp[i+1]++;
+        }else if (temp[i] === 3) {
+            temp[i] = 1;
+            temp[i+1]++;
+        }
+    }
+    
+    return Number(temp.reverse().join('')).toString();
 }
