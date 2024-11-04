65f3e4d3dcb1ee6dc467e2226e60e1062afbbb28
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-11-04 11:07:21 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,12 +1,14 @@
 function solution(my_str, n) {
-    const answer = [];
-    const len = my_str.length;
-    let i = 0;
+//     const answer = [];
+//     const len = my_str.length;
+//     let i = 0;
     
-    while (i < len) {
-        answer.push(my_str.slice(i, i+n));
-        i += n;
-    }
+//     while (i < len) {
+//         answer.push(my_str.slice(i, i+n));
+//         i += n;
+//     }
     
-    return answer;
+//     return answer;
+    
+    return my_str.match(new RegExp(`.{1,${n}}`, "g"));
 }
