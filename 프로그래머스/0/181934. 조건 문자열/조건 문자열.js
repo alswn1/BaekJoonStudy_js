db4913b4c2aeadcd7bd150b029cc7888046a795a
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-11-04 14:32:58 +0900

    [level 0] Title: 조건 문자열, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -1,7 +1,11 @@
 function solution(ineq, eq, n, m) {
-    if (ineq === ">") {
-        return eq === "=" ? +(n>=m) : +(n>m);
-    }else {
-        return eq === "=" ? +(n <= m) : +(n<m);
+    const operations = {
+        ">=" : (n, m) => n >= m,
+        "<=" : (n, m) => n <= m,
+        ">!" : (n, m) => n > m,
+        "<!" : (n, m) => n < m
     }
+    
+    const op = operations[ineq + eq];
+    return +(op(n, m))
 }
