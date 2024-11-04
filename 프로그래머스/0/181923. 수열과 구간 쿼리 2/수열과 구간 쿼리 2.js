13157fbeaf5a72f6b09eaa635c972c1692dc18d3
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-11-04 16:19:15 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 17.50 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -2,15 +2,8 @@ function solution(arr, queries) {
     const result = [];
     
     for (const [s, e, k] of queries) {
-        let min = Infinity;
-        
-        for (let i = s; i <= e; i++) {
-            if (arr[i] > k && arr[i] < min) {
-                min = arr[i];
-            }
-        }
-        
-        result.push(min === Infinity ? -1 : min);
+        let sliceArr = arr.slice(s, e+1).filter(e => e > k).sort((a, b) => a - b);
+        result.push(sliceArr[0] ? sliceArr[0] : -1);
     }
     return result;
 }
