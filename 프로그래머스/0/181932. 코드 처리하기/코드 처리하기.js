3d2b78cd489fd3984cf10d778a63ed2fdbaea9f8
Jade Bead <91466490+alswn1@users.noreply.github.com>
2024-11-06 15:08:01 +0900

    [level 0] Title: 코드 처리하기, Time: 11.65 ms, Memory: 39.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,15 @@
+function solution(code) {
+    let mode = 0;
+    let ret = '';
+    const len = code.length;
+    
+    for (let i=0; i<len; i++) {
+        if (code[i] === "1") {
+            mode = 1 - mode;
+        }else if ((mode === 0 && i % 2 === 0) || (mode === 1 && i % 2 === 1)) {
+            ret += code[i];
+        }
+    }
+
+    return ret || "EMPTY";
+}
